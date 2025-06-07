25b4ada3fba4aef863c47df99f7cc7aa94b1f821
HebsibaJerlinaPJ <p.j.hebsibajerlina@gmail.com>
2025-06-07 12:16:26 +0530

    cc

--- page/src/index.js
+++ page/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure this is correct
@@ -7,9 +7,13 @@ import './styles/EntryPage.css';
 import './styles/LoginPage.css';
 import './styles/Dashboard.css';
 
-ReactDOM.render(
+// Get the root DOM node
+const container = document.getElementById('root');
+
+// Create a root and render
+const root = createRoot(container);
+root.render(
   <Router>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
